ee91ab23ae6e25a1e81ce35511251018da2e9a2a
Benjamin Smith <bdsmith@ualberta.ca>
2021-04-12 11:48:28 -0600

    Planning out functionality for the TensorPropagator class

--- spinors/spinors.py
+++ spinors/spinors.py
@@ -15,6 +15,7 @@ class Spinors(object):
     def imaginary(self):
         ''' Also generates a dictionary of the tensor versions of all the energy
         grids; should be accessible to the user.'''
+        self.prop = TensorPropagator(self)
         return PropResult()
 
     def real(self):
@@ -53,7 +54,91 @@ class PropResult(object):
     def make_movie(self):
         pass
 
-class SpinCoupling(object):
-    def __init__(self):
+
+
+class TensorPropagator(object):
+    # Object that sucks in the needed energy grids and parameters for
+    # propagation, converts them to tensors, & performs the propagation.
+    #  - It means that two copies of the grids aren't carried in the main class
+    #  - However, it restricts access to the tensor form of the grids; unless
+    #    I keep the Propagation object as a class "attribute".
+    #  - I can directly pass `self` to this class and access class attributes,
+    #    methods, esp. energy grids. Only do this in the __init__ function
+    #    so as to not store the main Spinor object in the class
+    
+    # --> Should it be a class or just a pure function??
+    #      - Maybe a class because then it can store the grids it needs, and 
+    #        then access them from the different functions for free.
+    #      - It would allow these operations to reside in a separate module.
+    # BUT, then I have two classes who are attributes of each other, and
+    #     THAT's a weird structure.
+    #    - Maybe this class doesn't have to attribute the other one; it just
+    #      sucks in the data it needs.
+    
+    # Will need to calculate certain data throughout the loop, and then
+    #     create and populate the PropResult object.
+    
+    def __init__(self, spin):
+        # Needs:
+        #  - Energy grids
+        #  - Raman grids
+        #  - Atom number
+        #  - Number of steps
+        #  - grid parameters [don't keep tensor versions in a dict, not stable]
+        #  - dt
+        #  - sample (bool)
+        #  - wavefunction sample frequency
+        #  - wavefunction anneal frequency (imaginary time)
+        #  - device (cpu vs. gpu)
+        #  - 
+        
+        print(spin.N_STEPS)
+        pass
+
+
+    def evolution_op(self):
         pass
     
+    def coupling_op(self):
+        pass
+    
+    def single_step(self):
+        pass
+    
+    def full_step(self):
+        ''' Divides the full propagation step into three single steps using
+        the magic gamma for accuracy.
+        '''
+        self.single_step()
+        self.single_step()
+        self.single_step()
+        pass
+    
+
+    def propagation(self, N_STEPS):
+        '''Contains the actual propagation for-loop.'''
+        for i in range(N_STEPS):
+            self.full_step()
+        pass
+    
+    def energy_exp(self):
+        pass
+    
+    def normalize(self):
+        pass
+    
+    def density(self):
+        pass
+    
+    def inner_prod(self):
+        pass
+    
+    def expect_val(self):
+        pass
+    
+    
+    
+
+    
+    
+# Need a separate module with just tensor helper functions.
